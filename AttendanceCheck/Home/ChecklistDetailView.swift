98e69d31f638132c89593d279d77348ec05c0653
Rolf.J <kh.jang.1227@gmail.com>
2024-10-06 18:07:39 +0900

    Edit: Draft of HomeView

---
+++ AttendanceCheck/Home/ChecklistDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistDetailView.swift
+//  AttendanceCheck
+//
+//  Created by ROLF J. on 10/6/24.
+//
+
+import SwiftUI
+
+struct ChecklistDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChecklistDetailView()
+}
